313d0c3b8704fa2fff4b6ae7ed0ec446e5235f1c
Paul Molnar <saintpaul55012@yahoo.com>
2019-07-29 13:54:04 -0500

    footer created

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
-import './Footer.css'
+import './Footer.css';
+
+
+const boldStyle = {
+  fontWeight: 'bold',
+}
+
+const colorStyle = {
+  color: "#F46641",
+  textDecoration: "none",
+}
+
+const footerStyle = {
+    fontFamily: 'Libre Franklin',
+    color: "white",
+    backgroundColor: "#2C2A42",
+    borderTop: "1px solid #E7E7E7",
+    textAlign: "left",
+    fontSize: "12px",
+    paddingLeft: "40px",
+    paddingTop: "8px",
+    paddingBottom: '8px',
+    position: "fixed",
+    left: "10",
+    bottom: "0",
+    height: "42px",
+    width: "100%",
+    letterSpacing: ".0125em",
+    
+}
 
 class Footer extends Component {
   render() {
     return (
-      <div>
+      <div style = {footerStyle}>
         <p>
-          Footer
-        </p>
+           <span style= {boldStyle}>WithAll </span> 
+          • 5354 Parkdale Drive • Saint Louis Park, MN 55416
+          |  <span ><a style = {colorStyle} href= "www.withall.org" >  www.withall.org  </a></span>
+           | © 2019 All rights reserved.
+          </p>
+
+
       </div>
     );
   }
