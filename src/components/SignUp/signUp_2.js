22ed50c97ab43df531b659f58e4139f0f13dcb5f
Paul Molnar <saintpaul55012@yahoo.com>
2019-07-30 10:47:36 -0500

    sign-up-2

--- src/components/SignUp/signUp_2.js
+++ src/components/SignUp/signUp_2.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from './sig'
 
 class signUp_2 extends Component {
   render() {
     return (
       <div>
+        <Header width={'40%'}/><br/>
         <p>
           Info Page
         </p>
