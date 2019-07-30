16f11238024dfd957a2e08b98b807bcfbe5d84c0
Trevor Ramlow <trevorramlow@gmail.com>
2019-07-30 14:36:47 -0500

    create the basic outline for signup survey page 3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   HashRouter as Router,
   Route,
@@ -6,7 +6,7 @@ import {
   Switch,
 } from 'react-router-dom';
 
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
@@ -19,64 +19,74 @@ import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
 import signUp_1 from '../SignUp/signUp_1';
 
+import signUp_3 from '../SignUp/signUp_3';
 
 import './App.css';
 
 
+
 class App extends Component {
-  componentDidMount () {
-    this.props.dispatch({type: 'FETCH_USER'})
+  componentDidMount() {
+    this.props.dispatch({ type: 'FETCH_USER' })
   }
 
   render() {
     return (
       <Router>
-        
-          <Nav />
-          <Switch>
-            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-            <Redirect exact from="/" to="/home" />
-            {/* Visiting localhost:3000/about will show the about page.
+
+        <Nav />
+        <Switch>
+          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+          <Redirect exact from="/" to="/home" />
+          {/* Visiting localhost:3000/about will show the about page.
             This is a route anyone can see, no login necessary */}
-            <Route
-              exact
-              path="/about"
-              component={AboutPage}
-            />
-            {/* For protected routes, the view could show one of several things on the same route.
+          <Route
+            exact
+            path="/about"
+            component={AboutPage}
+          />
+          {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
             Even though it seems like they are different pages, the user is always on localhost:3000/home */}
-            <ProtectedRoute
-              exact
-              path="/home"
-              component={UserPage}
-            />
-            {/* This works the same as the other protected route, except that if the user is logged in,
+          <ProtectedRoute
+            exact
+            path="/home"
+            component={UserPage}
+          />
+          {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
-            <ProtectedRoute
-              exact
-              path="/info"
-              component={InfoPage}
-            />
-            <Route
-              exact
-              path="/signup1"
-              component={signUp_1}
-            />
+          <ProtectedRoute
+            exact
+            path="/info"
+            component={InfoPage}
+          />
+          <Route
+            exact
+            path="/signup1"
+            component={signUp_1}
+          />
 
 
 
 
 
+          <Route
+            exact
+            path="/signup3"
+            component={signUp_3}
+          />
 
-            {/* If none of the other routes matched, we will show a 404. */}
-            <Route render={() => <h1>404</h1>} />
-          </Switch>
-          <Footer />
-  
+
+
+          {/* If none of the other routes matched, we will show a 404. */}
+          <Route render={() => <h1>404</h1>} />
+        </Switch>
+        <Footer />
+
       </Router>
-  )}
+    )
+  }
 }
 
 export default connect()(App);
