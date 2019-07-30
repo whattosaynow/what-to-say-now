902fd22123b4bed39495341c3baad92befc3648a
Trevor Ramlow <trevorramlow@gmail.com>
2019-07-30 09:37:45 -0500

    buuldi the basic structure for page one of the signup survey

--- src/components/SignUp/signUp_1.js
+++ src/components/SignUp/signUp_1.js
@@ -3,11 +3,26 @@ import React, { Component } from 'react';
 class signUp_1 extends Component {
   render() {
     return (
+      <>
+      <header>
+        Thank you for participating in WithAll's "What To Say" Coaches Challenge. 
+        Please fill out the following form to participate.
+      </header>
       <div>
-        <p>
-          Info Page
-        </p>
+        <h2>Personal Information</h2><br />
+        <input label="First Name" placeholder="First Name"></input><br />
+        <input label="Last Name" placeholder="Last Name"></input><br />
+        <input label="Email" placeholder="Email"></input><br />
+        <input label="Password" placeholder="Password"></input><br />
+        <input label="Phone Number" placeholder="Phone Number"></input><br />
+        <input label="Street Address" placeholder="Street Address"></input><br />
+        <input label="City" placeholder="City"></input><br />
+        <input label="State" placeholder="State"></input><input label="Zip" placeholder="Zip"></input><br />
       </div>
+      <div>
+        <button>Next</button>
+      </div>
+      </>
     );
   }
 }
