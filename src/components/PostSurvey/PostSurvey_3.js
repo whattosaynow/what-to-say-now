b1da1aaceeca3f58fa42a60816853d798f875aa5
Paul Molnar <saintpaul55012@yahoo.com>
2019-08-01 11:28:58 -0500

    post survey 3 completed

--- src/components/PostSurvey/PostSurvey_3.js
+++ src/components/PostSurvey/PostSurvey_3.js
@@ -26,22 +26,29 @@ class PostSurvey_3 extends Component {
       alert("Please Answer All Questions")
     } else {
       this.props.dispatch({ type: `SET_POST_ANSWERS`, payload: this.state })
-    }
-
+      }
   }
+
+handleClickBack = () => {
+  console.log('back button clicked!');
+  this.props.history.push('/postsurvey2');
+}
 
   render() {
     return (
       <center>
         <div>
           <label> 9. What was your favorite thing about the Challenge?</label><br />
-          <textarea style={topMargin} rows='10' cols='100'></textarea><br />
+          <textarea onChange={this.handleChangeFor('favorite_thing')} style={topMargin} rows='10' cols='100'></textarea><br />
           <label>10.Can we call you for more information about your experience?</label><br />
-          <input style={topMargin} type="radio" value="Yes"></input>Yes<br />
-          <input type="radio" value="No"></input>No<br />
-          <button>Go Back</button>
-          <button>Submit</button>
+          <input onChange={this.handleChangeFor('call_more_information')} name='q10'style={topMargin} type="radio" value="Yes"></input>Yes<br />
+          <input onChange={this.handleChangeFor('call_more_information')} name='q10'type="radio" value="No"></input>No<br />
+          <button onClick={this.handleClickBack}>Go Back</button>
+          <button onClick={this.handleClick}>Submit</button>
       </div>
+      <pre>
+          {JSON.stringify(this.props.reduxState.answersReducer, null, 2)}
+        </pre>
       </center>
     );
   }
