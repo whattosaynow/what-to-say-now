6d38030cd3045f6245a2e82f5837fc080ceaaca4
Paul Molnar <saintpaul55012@yahoo.com>
2019-08-01 14:49:15 -0500

    database inserts

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import mountain from './mountain.svg';
 
 class UserPage extends Component {
   render() {
     return (
+      <center>
       <div>
-        <p>
-          User Page
-        </p>
+         <header className="sign-up-header">
+            View your weekly Challenges.<br />
+            <br />
+            <div className="outerBar" style={this.outerBar}>
+              <div className="innerBar" style={this.innerBar}></div>
+            </div>
+            <br />
+          </header>
+          <img src={mountain} alt='mountain' width='100px' height='100px'/>
+          <p>Weekly Challenge</p>
+          <div className='bottomDiv'>
+            <button onClick={this.handleClick}>Update My Accout Preferences</button>
+          </div>
       </div>
+      </center>
     );
   }
 }
