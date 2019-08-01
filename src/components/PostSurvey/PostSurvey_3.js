77e98c32039f49b41d664e79b94b33398d182a96
Paul Molnar <saintpaul55012@yahoo.com>
2019-08-01 11:05:26 -0500

    survey3

--- src/components/PostSurvey/PostSurvey_3.js
+++ src/components/PostSurvey/PostSurvey_3.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const topMargin = {
+  marginTop: '10px'
+}
 
 class PostSurvey_3 extends Component {
+
+  state = {}
+
+  handleChangeFor = (propertyName) => (event) => {
+    console.log(this.state);
+    this.setState({
+      ...this.state,
+      [propertyName]: event.target.value
+    });
+  }
+
+
+  handleClick = () => {
+    console.log('next button clicked!', this.state);
+    let survey2 = Object.keys(this.state);
+    console.log(survey2.length);
+    if (survey2.length < 2) {
+      alert("Please Answer All Questions")
+    } else {
+      this.props.dispatch({ type: `SET_POST_ANSWERS`, payload: this.state })
+    }
+
+  }
+
   render() {
     return (
-      <div>
-        <form>
-          9. What was your favorite thing about the Challenge?
-          <textarea rows="10" cols="50"></textarea>
-          10.Can we call you for more information about your experience?
-          <input type="radio" value="Yes">Yes</input>
-          <input type="radio" value="No">Yes</input>
+      <center>
+        <div>
+          <label> 9. What was your favorite thing about the Challenge?</label><br />
+          <textarea style={topMargin} rows='10' cols='100'></textarea><br />
+          <label>10.Can we call you for more information about your experience?</label><br />
+          <input style={topMargin} type="radio" value="Yes"></input>Yes<br />
+          <input type="radio" value="No"></input>No<br />
           <button>Go Back</button>
           <button>Submit</button>
-        </form>
       </div>
+      </center>
     );
   }
 }
 
-export default PostSurvey_3;
+const mapStateToProps = (reduxState) => ({
+  reduxState,
+})
+
+export default connect(mapStateToProps)(PostSurvey_3);
