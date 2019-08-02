0bd0a4ea1ec404bff76cbbd1ad122693b563bf27
Kellen Baker <mkellenbaker@gmail.com>
2019-08-02 15:04:40 -0500

    add rejectUnauthenticated to routes

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-
+const {rejectUnauthenticated} = require("../modules/authentication-middleware");
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('admin router get hit')
     pool.query(`
     SELECT * FROM "content";
