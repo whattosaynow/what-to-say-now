d4bd58e72126d1adead791db3227aad670c1c937
Trevor Ramlow <trevorramlow@gmail.com>
2019-08-02 11:03:44 -0500

    add onClick function to select role, age, and week

--- src/components/Admin/AdminEdit.js
+++ src/components/Admin/AdminEdit.js
@@ -21,15 +21,15 @@ class AdminDetails extends Component {
       <>
         <header>
           <center><button onClick={this.handleClick('role')} value="1">Coach</button><button onClick={this.handleClick('role')} value="2">Pediatrician</button><button onClick={this.handleClick('role')} value="3">Parents</button></center><br />
-          <center><button>Ages 6-9</button><button>Ages 10-13</button><button>Ages 14-18</button></center>
+          <center><button onClick={this.handleClick('age')} value="1">Ages 6-9</button><button onClick={this.handleClick('age')} value="2">Ages 10-13</button><button onClick={this.handleClick('age')} value="3">Ages 14-18</button></center>
         </header>
         <div>
           <ul>
-            <li><button>Week 1</button></li>
-            <li><button>Week 2</button></li>
-            <li><button>Week 3</button></li>
-            <li><button>Week 4</button></li>
-            <li><button>Week 5</button></li>
+            <li><button onClick={this.handleClick('week')} value="1">Week 1</button></li>
+            <li><button onClick={this.handleClick('week')} value="2">Week 2</button></li>
+            <li><button onClick={this.handleClick('week')} value="3">Week 3</button></li>
+            <li><button onClick={this.handleClick('week')} value="4">Week 4</button></li>
+            <li><button onClick={this.handleClick('week')} value="5">Week 5</button></li>
           </ul>
         </div>
         <div></div>
