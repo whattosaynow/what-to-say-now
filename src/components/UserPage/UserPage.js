e1f553a21ee20034efe9aeb2e54aff15d967eb5d
Paul Molnar <saintpaul55012@yahoo.com>
2019-08-02 10:28:13 -0500

    semantic-ui-card

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 class UserPage extends Component {
   render() {
     return (
-      <div>
-        <p>
-          User Page
-        </p>
-      </div>
+      // const CardExampleCard = () => (
+        <Card>
+          <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>Matthew</Card.Header>
+            <Card.Meta>
+              <span className='date'>Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>
+              Matthew is a musician living in Nashville.
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              22 Friends
+            </a>
+          </Card.Content>
+        </Card>
+      // )
+      // <div>
+      //   <p>
+      //     User Page
+      //   </p>
+      // </div>
     );
   }
 }
