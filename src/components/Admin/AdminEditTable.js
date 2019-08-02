8829fb6522ae1d2f49606d33a834ae26a7271395
Trevor Ramlow <trevorramlow@gmail.com>
2019-08-02 14:26:57 -0500

    update the dit view have the inputs on load

--- src/components/Admin/AdminEditTable.js
+++ src/components/Admin/AdminEditTable.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 
 class AdminEditTable extends Component {
+  state={
+    ...this.props.content
+  }
+
+  handleChange = (properyName) => (event) => {
+    this.setState({
+      [properyName]: event.target.value
+    })
+  }
   render() {
     return (
       <>
-        <li>Week {this.props.content.week}</li><br />
+      {/* <pre>
+        {JSON.stringify(this.state, null, 2)}
+      </pre> */}
+        <li>Week {this.state.week}</li><br />
         This week's "What to Say" phrase is:<br />
-        <textarea rows="10" cols="200" value={this.props.content.intro}></textarea><br />
+        <textarea rows="10" cols="200" onChange={this.handleChange('intro')} value={this.state.intro}></textarea><br />
         <br />
         Why does this phrase matter?<br />
-        <textarea rows="10" cols="200" value={this.props.content.why_matters}></textarea><br />
+        <textarea rows="10" cols="200" onChange={this.handleChange('why_matters')} value={this.state.why_matters}></textarea><br />
         <br />
         For Your Own Reflection<br />
-        <textarea rows="10" cols="200" value={this.props.content.reflection}></textarea><br />
+        <textarea rows="10" cols="200" onChange={this.handleChange('reflection')} value={this.state.reflection}></textarea><br />
         <br />
         Action Step Options<br />
-        <textarea rows="10" cols="200" value={this.props.content.action_steps}></textarea><br />
+        <textarea rows="10" cols="200" onChange={this.handleChange('action_steps')} value={this.state.action_steps}></textarea><br />
       </>
     );
   }
