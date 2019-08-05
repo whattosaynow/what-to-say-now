7d7b2f891c46547dbee8e431e753536d2525c30e
Trevor Ramlow <trevorramlow@gmail.com>
2019-08-05 16:22:32 -0500

    allow the admin to click to download a csv

--- src/components/Admin/AdminDetails.js
+++ src/components/Admin/AdminDetails.js
@@ -3,11 +3,45 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
 class AdminDetails extends Component {
+
+  objectToCsv = (data) => {
+    const csvRows = [];
+
+    //get headers
+    const headers = Object.keys(data[0]);
+    csvRows.push(headers.join(','));
+
+    //loop over the rows
+    for (const row of data) {
+      const values = headers.map(header => {
+        const escaped = (''+row[header]).replace(/"/g, '\\"')
+        return `"${escaped}"`;
+      })
+      csvRows.push(values.join(','))
+    }
+
+    //form escaped comma separated values
+    return this.download(csvRows.join('\n'));
+  }
+
+  download = (data) => {
+    const blob = new Blob([data], {type: 'text/csv'});
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.setAttribute('hidden', '');
+    a.setAttribute('href', url);
+    a.setAttribute('download', 'download.csv');
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+  }
+
   render() {
     return (
       <div>
         <p>
-          <button>Click here to download CSV of user data</button>
+          <button onClick={() => this.objectToCsv(this.props.reduxState.adminReducer.csvReducer)}>Click here to download CSV of user data</button>
         </p>
       </div>
     );
