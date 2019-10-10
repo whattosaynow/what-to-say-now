48bb957b36a324efccef9bbdfa7192d42bcede0d
Trevor Ramlow <trevorramlow@gmail.com>
2019-10-10 15:49:39 -0500

    create a page that loads only if there is a token that matches the code

--- src/components/PasswordReset/PasswordReset.js
+++ src/components/PasswordReset/PasswordReset.js
@@ -10,36 +10,57 @@ class PasswordReset extends Component {
         confirmNewPassword: ''
     };
 
+    handleChangeFor = (propertyName) => (event) => {
+        this.setState({
+            ...this.state,
+            [propertyName]: event.target.value
+        });
+    }
 
 
     render() {
-        return (
-            <div><br />
-                <div className="signup-questions"><br />
-                    <center>
-                        <h1>Update Preferences</h1>
-                        <br />
-                        <span>New Password:</span> <br />
-                        <Input></Input><br />
-                        <br />
-                        <span>Confirm New Password:</span> <br />
-                        <Input></Input><br />
-                        <br />
-                        {this.state.newPassword === this.state.confirmNewPassword ? 
-                        <>
-                        <Button>Submit</Button><br />
-                        </>
-                        :
-                        <>
-                        <span style={{color: "red"}}>ERROR: Passwords don't match</span>
-                        <br />
-                        </>
-                        }
-                        <br />
-                    </center>
+        if (this.props.match.params.token === 'apple') {
+            return (
+                <div><br />
+                    <div className="signup-questions"><br />
+                        <center>
+                            <h1>Update Preferences</h1>
+                            <br />
+                            <span>New Password:</span> <br />
+                            <Input type="password" onChange={this.handleChangeFor('newPassword')} ></Input><br />
+                            <br />
+                            <span>Confirm New Password:</span> <br />
+                            <Input type="password" onChange={this.handleChangeFor('confirmNewPassword')} ></Input><br />
+                            <br />
+                            {this.state.newPassword === this.state.confirmNewPassword ?
+                                <>
+                                    <Button>Submit</Button><br />
+                                </>
+                                :
+                                <>
+                                    <span style={{ color: "red" }}>ERROR: Passwords don't match</span>
+                                    <br />
+                                </>
+                            }
+                            <br />
+                        </center>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+            return (
+                <>
+                <br />
+                <div>
+                    Error - INVALID TOKEN<br />
+                    Please request new  token
+                    {/* <pre>
+                        {JSON.stringify(this.props.match.params.token, null, 2)}
+                    </pre> */}
+                    </div>
+            </>
+            )
+        }
     }
 }
 
