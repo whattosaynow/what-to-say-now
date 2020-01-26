dffa78c5d899917d17210a2c906d3f45e89bc4fd
Trevor Ramlow <trevorramlow@gmail.com>
2020-01-26 11:18:25 -0600

    finished updating styling for sign up

--- src/components/SignUp/signUp_4.js
+++ src/components/SignUp/signUp_4.js
@@ -64,89 +64,106 @@ class signUp_4 extends Component {
     return (
       <>
         <Header width={'80%'} /><br />
-        <center><h2>Personal Information Continued</h2></center><br />
+        <center>
+          <h2>Personal Information Continued</h2>
+        </center>
+        <br />
         <div className="signup-card"><br />
-          <span className="survey-questions">7. Age(s) you coach and want to focus on with during the Challenge:</span><br />
-          <label className="question-label">choose one*</label><br />
-          <Input
-            onChange={this.handleChange('focus_ages')}
-            type="radio"
-            className="radio-button"
-            checked={this.state.focus_ages === '1'}
-            name="q1"
-            value="1"
-            id="ques7answer1"
-            /><label className="survey-answers" for="ques7answer1">Elementary school youth (6-10 years old)</label><br />
+          <span className="survey-questions">7. Age(s) you coach and want to focus on with during the Challenge:</span>
+          <label className="question-label">choose one*</label>
+          <div class="radio-answer-pair">
+            <input
+              onChange={this.handleChange('focus_ages')}
+              type="radio"
+              className="radio-button"
+              checked={this.state.focus_ages === '1'}
+              name="q1"
+              value="1"
+              id="ques7answer1"
+            /><label className="survey-answers" for="ques7answer1">Elementary school youth (6-10 years old)</label>
+          </div>
 
-          <Input
-            onChange={this.handleChange('focus_ages')}
-            type="radio"
-            className="radio-button"
-            checked={this.state.focus_ages === '2'}
-            name="q1"
-            value="2"
-            id="ques7answer2"
-            /><label className="survey-answers"  for="ques7answer2">Middle school youth (10-13 years old)</label><br />
+          <div class="radio-answer-pair">
+            <input
+              onChange={this.handleChange('focus_ages')}
+              type="radio"
+              className="radio-button"
+              checked={this.state.focus_ages === '2'}
+              name="q1"
+              value="2"
+              id="ques7answer2"
+            /><label className="survey-answers" for="ques7answer2">Middle school youth (10-13 years old)</label>
+          </div>
 
-          <Input
-            onChange={this.handleChange('focus_ages')}
-            type="radio"
-            className="radio-button"
-            checked={this.state.focus_ages === '3'}
-            name="q1"
-            value="3"
-            id="ques7answer3"
-            /><label className="survey-answers"  for="ques7answer3">High school youth (13-18 years old)</label><br />
-          <i><label className="survey-questions">*We realize some of you may coach multiple age groups or may not be a coach, however,
+          <div class="radio-answer-pair">
+            <input
+              onChange={this.handleChange('focus_ages')}
+              type="radio"
+              className="radio-button"
+              checked={this.state.focus_ages === '3'}
+              name="q1"
+              value="3"
+              id="ques7answer3"
+            /><label className="survey-answers" for="ques7answer3">High school youth (13-18 years old)</label>
+          </div>
+          <i><label className="question-label exemption">*We realize some of you may coach multiple age groups or may not be a coach, however,
             the Coaches Challenge is setup to focus on one age group at a time, so please choose one age group.
             If you’d like to focus on a different age group in the future, please take the Challengeagain.</label></i><br />
           <br />
 
           <span className="survey-questions">8. Your answer to this question informs WithAll for future programming. Select all that apply:</span> <br />
-          <Input
-            onChange={this.handleCheckBox('parent_or_guardian')}
-            type="checkbox"
-            checked={!!this.state.parent_or_guardian}
-            className="radio-button"
-            name="q2"
-            value={true} 
-            id="ques8answer1"
-            /><label className="survey-answers"  for="ques8answer1">I’m a parent or guardian of a child between 0 and 18 years old</label><br />
+          <div class="radio-answer-pair">
+            <input
+              onChange={this.handleCheckBox('parent_or_guardian')}
+              type="checkbox"
+              checked={!!this.state.parent_or_guardian}
+              className="radio-button"
+              name="q2"
+              value={true}
+              id="ques8answer1"
+            /><label className="survey-answers" for="ques8answer1">I’m a parent or guardian of a child between 0 and 18 years old</label>
+          </div>
 
-          <Input
-            onChange={this.handleCheckBox('healthcare_professional')}
-            type="checkbox"
-            checked={!!this.state.healthcare_professional}
-            className="radio-button"
-            name="q2"
-            value={true} 
-            id="ques8answer2"
-            /><label className="survey-answers"  for="ques8answer2">I’m health care professional</label><br />
+          <div class="radio-answer-pair">
+            <input
+              onChange={this.handleCheckBox('healthcare_professional')}
+              type="checkbox"
+              checked={!!this.state.healthcare_professional}
+              className="radio-button"
+              name="q2"
+              value={true}
+              id="ques8answer2"
+            /><label className="survey-answers" for="ques8answer2">I’m health care professional</label>
+          </div>
 
-          <Input
-            onChange={this.handleCheckBox('teacher')}
-            type="checkbox"
-            checked={!!this.state.teacher}
-            className="radio-button"
-            name="q2"
-            value={true} 
-            id="ques8answer3"
-            /><label className="survey-answers"  for="ques8answer3">I’m a teacher in early education, elementary school, middle school, or high school</label><br />
+          <div class="radio-answer-pair">
+            <input
+              onChange={this.handleCheckBox('teacher')}
+              type="checkbox"
+              checked={!!this.state.teacher}
+              className="radio-button"
+              name="q2"
+              value={true}
+              id="ques8answer3"
+            /><label className="survey-answers" for="ques8answer3">I’m a teacher in early education, elementary school, middle school, or high school</label>
+          </div>
 
-          <Input
-            onChange={this.handleCheckBox('none_above')}
-            type="checkbox"
-            checked={!!this.state.none_above}
-            className="radio-button"
-            name="q2"
-            value={true} 
-            id="ques8answer4"
-            /><label className="survey-answers"  for="ques8answer4">None of the above</label><br />
+          <div class="radio-answer-pair">
+            <input
+              onChange={this.handleCheckBox('none_above')}
+              type="checkbox"
+              checked={!!this.state.none_above}
+              className="radio-button"
+              name="q2"
+              value={true}
+              id="ques8answer4"
+            /><label className="survey-answers" for="ques8answer4">None of the above</label>
+          </div>
 
           <br />
 
           <span className="survey-questions">9. What youth sports organizations are you or your team affiliated with?</span> <br />
-          <label className="question-label">If none, reply with “none.”</label><br />
+          <label className="question-label">If none, reply with “none.”</label>
           <Input
             name="sports_org"
             className="radio-button"
